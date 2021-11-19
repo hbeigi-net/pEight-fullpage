6a2eef0200444a223214014b7940da8176b4dceb
hamid <hbeigi57@gmail.com>
2021-11-19 15:45:38 +0330

    crate all section and compelete section one with slides

---
+++ assets/js/code.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+	
+	autoScrolling:true,
+	scrollHorizontally: true,
+    scrollBar: false,
+    navigation: true,
+    navigationTooltips: ["صفحه اول ","صفحه دوم","صفحه سوم","صفحه چهارم","صفحه پنجم "],
+    showActiveTooltip: true
+});
+fullpage_api.setAllowScrolling(true);
